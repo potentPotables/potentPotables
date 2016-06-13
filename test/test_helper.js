dcfac573706b5cb3f3da643102a756959d993441
Peter <peter.dinh@gmail.com>
2016-06-12 18:53:46 -0700

    [progress] renderComponent looks good

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,7 +1,25 @@
+import React from 'react';
 import jsdom from 'jsdom';
 import _$ from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../src/reducers';
 
-global.document = jsdom.jsdom('<!doctype html><html><body></body></html');
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 const $ = _$(global.window);
 
+function renderComponent(ComponentClass, props, state) {
+	const componentInstance = TestUtils.renderIntoDocument(
+		<Provider store={createStore(reducers, state)}>
+			<ComponentClass {...props} />
+		</Provider>
+	);
+
+	 return $(ReactDOM.findDOMNode(componentInstance));
+}
+
+export { renderComponent, expect };
