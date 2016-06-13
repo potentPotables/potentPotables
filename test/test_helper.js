c2237cebb4eda601e2aa14e37622026ce9ba408c
Peter <peter.dinh@gmail.com>
2016-06-12 19:09:56 -0700

    [feature] test helper complete, npm run test -- --watch

--- test/test_helper.js
+++ test/test_helper.js
@@ -3,10 +3,11 @@ import jsdom from 'jsdom';
 import _$ from 'jquery';
 import TestUtils from 'react-addons-test-utils';
 import ReactDOM from 'react-dom';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
+import chaijQuery from 'chai-jquery';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
@@ -21,5 +22,15 @@ function renderComponent(ComponentClass, props, state) {
 
 	 return $(ReactDOM.findDOMNode(componentInstance));
 }
+
+$.fn.simulate = function(eventName, value) {
+	if(value) {
+		this.val(value);
+	}
+
+	TestUtils.Simulate[eventName](this[0]);
+}
+
+chaijQuery(chai, chai.util, $)
 
 export { renderComponent, expect };
