40e4603b6a40e0065bd27bbfec1063d03febc8cb
Peter <peter.dinh@gmail.com>
2016-06-12 22:28:40 -0700

    [progress] creates test for game_board reducer

--- test/reducers/gameboard_test.js
+++ test/reducers/gameboard_test.js
@@ -1,8 +1,16 @@
 import { expect } from '../test_helper';
-import gameboard_reducer, {} from '../../src/reducers/gameboard_reducer';
-import { CREATE_ GAME } from '../../src/actions/index';
+import gameboardReducer from '../../src/reducers/gameboard_reducer';
+import { CREATE_GAME } from '../../src/actions/index';
 
 describe('Gameboard Reducer', () => {
 
-	it('handles action that are')
+	it('handles actions that are !CREATE_GAME', () => {
+		expect(gameboardReducer({}, {})).to.eql({});
+	});
+
+	it('handles actions that are CREATE_GAME', () => {
+		const action = {type: CREATE_GAME, payload: {categories: [], clues: []}};
+		expect(gameboardReducer({}, action)).to.eql({categories: [], clues: []});
+	});
+
 })
