124001dc3eb4fe883b89b91ba8ea129844e3d756
Chris Loncarich <loncarichchris@gmail.com>
2016-06-20 10:51:15 -0700

    [bug] Fixes Port Variable server

--- server/server.js
+++ server/server.js
@@ -38,6 +38,6 @@ io.on('connection', function(socket){
 });
 //Server set up
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 server.listen(port);
 console.log('listening on port:', port);
