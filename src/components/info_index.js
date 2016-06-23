77d2a5b417266031bc5cd1d62c50858dbe1e7e4d
lstuartfry <lstuartfry@gmail.com>
2016-06-23 15:21:36 -0700

    [add] adds index landing logo and info transitions

--- src/components/info_index.js
+++ src/components/info_index.js
@@ -2,25 +2,33 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { createSession } from '../actions/index';
 import { Link } from 'react-router';
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+
 
 class InfoIndex extends Component {
 	render() {
 
 		return (
 			<div className ="landing">
-				<div className="logo-holder"><img id="logo" src="http://52.38.175.65/assets/logo_glow.png"/></div>
-				<div>
-				<button className="create btn btn-primary btn-lg" onClick={this.props.createSession}>Create a session</button>
-				</div>
-				<div>
-				<Link to="/linkverification" className="join btn btn-primary btn-lg">Join a session</Link>
-				</div>
-				<span>
-					<Link className="about" to="/about"><strong><u>About</u></strong></Link>
-				</span>
-				<span>
-					<Link className="howto" to="/howto"><strong><u>How to Play</u></strong></Link>
-				</span>
+				<ReactCSSTransitionGroup transitionName="logo-transition" transitionAppear={true} transitionAppearTimeout={1500}>
+					<div className="logo-holder"><img id="logo" src="./assets/logo.png"/></div>
+				</ReactCSSTransitionGroup>
+				<ReactCSSTransitionGroup transitionName="index-transition" transitionAppear={true} transitionAppearTimeout={3000}>
+					<div>
+						<button className="create btn btn-primary btn-lg" onClick={this.props.createSession}>Create a session</button>
+					</div>
+				</ReactCSSTransitionGroup>
+				<ReactCSSTransitionGroup transitionName="index-transition" transitionAppear={true} transitionAppearTimeout={3000}>
+					<div>
+						<Link to="/linkverification" className="join btn btn-primary btn-lg">Join a session</Link>
+					</div>
+					<span>
+						<Link className="about" to="/about"><strong><u>About</u></strong></Link>
+					</span>
+					<span>
+						<Link className="howto" to="/howto"><strong><u>How to Play</u></strong></Link>
+					</span>
+				</ReactCSSTransitionGroup>
 			</div>
 		);
 	}
