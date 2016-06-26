815b405264acb2f168d8b788ab5611b6139564c4
Peter <peter.dinh@gmail.com>
2016-06-26 13:31:51 -0700

    [style] bio pages centered

--- src/components/peter.js
+++ src/components/peter.js
@@ -5,15 +5,17 @@ export default class Peter extends Component {
 	render() {
 		return (
 			<div className="bio">
-				<h1>Peter Dinh</h1>
+				<h1 className="devName peter">Peter Dinh</h1>
 				<img />
-				<div>
-					<a href="https://github.com/petertdinh"><img src="http://52.38.175.65/tech_logos/github.png" height="45" width="120"/></a>
+				<div className="externals">
+					<div className="github">
+						<a href="https://github.com/petertdinh"><img src="http://52.38.175.65/tech_logos/github.png" height="45" width="120"/></a>
+					</div>
+					<div className="linkedin">
+						<a href="https://www.linkedin.com/in/petertdinh"><img src="http://52.38.175.65/tech_logos/linkedin.png" height="30" width="120"/></a>
+					</div>
 				</div>
-				<div>
-					<a href="https://www.linkedin.com/in/petertdinh"><img src="http://52.38.175.65/tech_logos/linkedin.png" height="30" width="120"/></a>
-				</div>
-				<div>Something that screams hire me here.</div>
+				<div className="sell">Something that screams hire me here.</div>
 				<Link to="hire/peter">
 					Hire Peter
 				</Link>
