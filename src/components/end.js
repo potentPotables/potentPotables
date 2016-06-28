e10faaf9f2a35183ab69f20525ae825136879563
Peter <peter.dinh@gmail.com>
2016-06-28 14:22:10 -0700

    [style] audio for end view

--- src/components/end.js
+++ src/components/end.js
@@ -2,21 +2,30 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 
-export default class End extends Component {
+class End extends Component {
+
+	componentDidMount() {
+		const end = new Audio('http://www.orangefreesounds.com/wp-content/uploads/2014/10/Jeopardy-theme-song.mp3');
+		end.play();
+	}
 
 	render(){
 		const scores = _.map(this.props.users, function(user) {
 			return (
-				<div key={user.username}>
-					{user.username}: ${user.score}
+				<div className="report" key={user.username}>
+						<span className="finaluser">{user.username}:</span>
+						<span className="finalscore">{user.score > 0 ? '$' + user.score : '-$' + user.score}</span>
 				</div>
 			);
 		});
 
 	  return (
-	      <div>
-	      	<h1>Thank you for playing!</h1>
-	      	{ scores }
+	      <div id="end">
+	      	<h1 className="thankyou animated infinite flash">Thank you for playing!</h1>
+	      	<div className="final header"><strong>Final scores:</strong></div>
+	      	<div className="final scores">
+	      		{ scores }
+	      	</div>
 	      </div>
 	  );
 	}
