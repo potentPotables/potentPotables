0cfb351350cb714757d3579eda2d803a65444a73
lstuartfry <lstuartfry@gmail.com>
2016-06-30 17:37:42 -0700

    [progress] send message page layout

--- src/components/hire_peter.js
+++ src/components/hire_peter.js
@@ -16,36 +16,38 @@ class HirePeter extends Component {
 		const { fields: { name, company, email, message }, handleSubmit } = this.props;
 
 		return (
+			<div className="hireContainer">
+			<div className="hireLayout">
 			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-			  <h3>Send me a message.</h3>
-			  <div className={`form-group ${name.touched && name.invalid ? 'has-danger' : ''}`}>
-			    <label><strong>Name</strong></label>
-			    <input type="text" className="form-control" {...name} />
+			  <h3 style={{color: "yellow", fontFamily: "Swiss-911", fontSize: "3em"}}>Send me a message</h3>
+			  <div className={`form-group ${name.touched && name.invalid ? 'has-danger' : ''}`} style={{height: "5vh", width: "80%", margin: "auto", marginBottom: "10%"}}>
+			    <label><strong style={{fontSize: "2em"}}>Name</strong></label>
+			    <input type="text" className="form-control hire-input" {...name} />
 			    <div class="text-help">
 			      {name.touched ? name.error: ''}
 			    </div>
 			  </div>
 
-			  <div className={`form-group ${company.touched && company.invalid ? 'has-danger' : ''}`}>
-			    <label><strong>Company</strong></label>
-			    <input type="text" className="form-control" {...company} />
+			  <div className={`form-group ${company.touched && company.invalid ? 'has-danger' : ''}`} style={{height: "5vh", width: "80%", margin: "auto", marginBottom: "10%"}}>
+			    <label><strong style={{fontSize: "2em"}}>Company</strong></label>
+			    <input type="text" className="form-control hire-input" {...company} />
 			    <div class="text-help">
 			      {company.touched ? company.error: ''}
 			    </div>
 			  </div>
 
 
-			  <div className={`form-group ${email.touched && email.invalid ? 'has-danger' : ''}`}>
-			    <label><strong>Email</strong></label>
-			    <input type="email" className="form-control" {...email} />
+			  <div className={`form-group ${email.touched && email.invalid ? 'has-danger' : ''}`} style={{height: "5vh", width: "80%", margin: "auto", marginBottom: "10%"}}>
+			    <label><strong style={{fontSize: "2em"}}>Email</strong></label>
+			    <input type="email" className="form-control hire-input" {...email} />
 			    <div class="text-help">
 			      {email.touched ? email.error: ''}
 			    </div>
 			  </div>
 			  
-			  <div className={`form-group ${message.touched && message.invalid ? 'has-danger' : ''}`}>
-			    <label><strong>Message</strong></label>
-			    <textarea className="form-control" {...message} />
+			  <div className={`form-group ${message.touched && message.invalid ? 'has-danger' : ''}`} style={{height: "5vh", width: "80%", margin: "auto", marginBottom: "10%"}}>
+			    <label><strong style={{fontSize: "2em"}}>Message</strong></label>
+			    <input type="text" className="form-control hire-input" {...message} />
 			    <div class="text-help">
 			      {message.touched ? message.error: ''}
 			    </div>
@@ -53,6 +55,8 @@ class HirePeter extends Component {
 
 			  <button type="submit" className="btn btn-primary">Submit</button>
 			</form>
+			</div>
+			</div>
 		);
 	}
 }
